6538a52be7f53873b6bff61cc69d3ab65e137f9a
Michal Klimuk <michal.klimuk@neo9.pl>
2015-06-26 16:00:35 +0200

    enabled live reload

--- install.sh
+++ install.sh
@@ -1,4 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 sudo cp build-tools/*.sh /usr/local/bin
-cp .docker-aliases ~/.docker-aliases
-cat '.docker-aliases' >> ~/.bashrc
+
+BASHRC_PATH=~/.bashrc
+ALIASES_PATH=~/.docker-aliases
+
+read -d '' aliases <<- EOF
+alias gradle='gradle-build.sh'
+alias mvn='mvn-build.sh'
+alias drm='docker rm $(docker ps -aq)'
+alias drmi-dang='docker rmi $(docker images -q --filter "dangling=true")'
+EOF
+
+echo "$aliases" >> "$ALIASES_PATH"
+chmod +x "$ALIASES_PATH"
+echo "$ALIASES_PATH" >> "$BASHRC_PATH"
+
+$ALIASES_PATH
